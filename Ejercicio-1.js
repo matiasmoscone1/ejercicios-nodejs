4acadc0745ef49d1ef84ee36889d50d2c08aff14
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-07-14 15:33:58 -0300

    agregando rutas al ejercicio-1

--- Ejercicio-1.js
+++ Ejercicio-1.js
@@ -2,7 +2,25 @@ const http = require("node:http");
 
 
 const server = http.createServer((req, res) => {
-    res.end("Hola Mundo!");
+    //res.end("Hola Mundo!");
+
+    if(req.url === "/"){
+        req.statusCode = 200;
+        req.end("Bienvenidos al inicio!");
+    }else if(req.url === "/Contacto"){
+        req.statusCode = 200;
+        req.end("Bienvenidos a la seccion Contacto!");
+    }else if(req.url === "/Sobre"){
+        req.statusCode = 200;
+        req.end("Bienvenidos a la seccion Sobre Nosotros!");
+    }else if(req.url === "/Info"){
+        req.statusCode = 200;
+        req.end("Bienvenidos a la seccion Informacion!");
+    }
+
+
+
+
 });
 
 const port = 3000;
