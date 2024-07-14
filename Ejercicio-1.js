e4bf2cb6563e0d5f86b7c289ef486abf80c6b4bf
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-07-14 14:55:14 -0300

    Ejercicio 1 terminado, ver que se le puede agregar

--- Ejercicio-1.js
+++ Ejercicio-1.js
@@ -5,9 +5,10 @@ const server = http.createServer((req, res) => {
     res.end("Hola Mundo!");
 });
 
+const port = 3000;
 
-server.listen(3000, () => {
-    
+server.listen(port, () => {
+    console.log(`El servidor se esta ejecutando en el puerto: ${port}`);
 });
 
 
