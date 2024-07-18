6a3d436b6e3eea8c9328d2adbbe56489c27e121b
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-07-18 11:34:39 -0300

    Mejorando codigo del ejercicio 3

--- ejercicio-3.js
+++ ejercicio-3.js
@@ -1,16 +1,18 @@
 /* Contador de Visitas: Crea un servidor que cuente el número de veces que se ha 
 accedido a la página y muestre ese número. */ 
 
-const { count } = require("node:console");
 const http = require("node:http");
 
-const count = 0;
 
+let count = 0;
 const server = http.createServer((req, res) => {
-    if(res.statusCode === 200){
+
+    if(req.url === "/"){
         count++;
-    }    
-    res.end(`Las veces que se visito esta pagina son: ${count}`);
+        res.end(`Las veces que se visito esta pagina son: ${count}`);
+    }
+
+
 });
 
 const port = 3000;
@@ -18,5 +20,7 @@ const port = 3000;
 server.listen(port, () => {
     console.log(`El servidor esta siendo escuchado en el puerto: ${port}`);
 });
+
+
 
 
