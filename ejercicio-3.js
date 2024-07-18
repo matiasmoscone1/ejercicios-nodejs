4c863c93eb099cf58971ec706ead3307304267d0
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-07-18 09:36:24 -0300

    Creando estructura del ejercicio 3

--- ejercicio-3.js
+++ ejercicio-3.js
@@ -1,7 +1,17 @@
-/* Contador de Visitas: Crea un servidor que cuente el número de veces que se ha accedido a la página y 
-muestre ese número. */ 
+/* Contador de Visitas: Crea un servidor que cuente el número de veces que se ha 
+accedido a la página y muestre ese número. */ 
 
+const http = require("node:http");
 
+const server = http.createServer((req, res) => {
+    
 
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+    console.log(`El servidor esta siendo escuchado en el puerto: ${port}`);
+});
 
 
