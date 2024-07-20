29544e2aeefb6e123d7e48d73a97789ffb8ae6b4
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-07-20 12:18:04 -0300

    Mejorando el ejercicio 4

--- ejercicio-4.js
+++ ejercicio-4.js
@@ -2,16 +2,41 @@
 Crea un programa que escriba un mensaje en un archivo llamado mensaje.txt. */
 
 const fs = require("node:fs");
+const rl = require("node:readline");
 
 
-fs.writeFile("mensaje.txt", "Hola muy buenos dias!!!!", (err) => {
-    if(err){
-        console.log("no se encontro el archivo...");
+const interface = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+interface.question("Sobreescribir archivo (s) - Añadir informacion (a)", () => {
+    if("s"){
+        fs.writeFile("mensaje.txt", "Hola muy buenos dias!!!!", (err) => {
+            if(err){
+                console.log("no se encontro el archivo...");
+            }
+        })
+    }
+    else if("a"){
+        const string = "";
+        interface.question("Escriba lo que quiere añadir: ", () => {
+            string = 
+        })
+        fs.appendFile("mensaje.txt", "\nEstoy añadiendo informacion al arhivo :)", (err) => {
+            if(err){
+                console.log("no se encuentra el archivo...");
+            }
+        });
     }
 })
 
-fs.appendFile("mensaje.txt", "\nEstoy añadiendo informacion al arhivo :)", (err) => {
-    if(err){
-        console.log("no se encuentra el archivo...");
-    }
-});
+
+
+
+
+
+
+
+
+
