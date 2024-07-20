d0f2da3d8f49131135a0f06ec84e4c38a1db6254
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-07-20 12:07:00 -0300

    resolviendo ejercicio-4

--- ejercicio-4.js
+++ ejercicio-4.js
@@ -4,9 +4,10 @@ Crea un programa que escriba un mensaje en un archivo llamado mensaje.txt. */
 const fs = require("node:fs");
 
 
-
-
-
-
+fs.writeFile("mensaje.txt", "Hola muy buenos dias!!!!", (err) => {
+    if(err){
+        console.log("no se encontro el archivo...");
+    }
+})
 
 
