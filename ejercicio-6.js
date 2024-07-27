2696aa5ac5ad5a5b738ce5d2afd9916315569045
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-07-27 10:57:03 -0300

    Realizando ejercicio 6

--- ejercicio-6.js
+++ ejercicio-6.js
@@ -2,6 +2,14 @@
 Crea un programa que elimine nuevoMensaje.txt o algun archivo que se le pase */
 
 const fs = require("node:fs");
+const rl = require("node:readline");
+
+const interface = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
 
 
 
