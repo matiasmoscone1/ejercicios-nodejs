bdf3e0867bd3f7faade8a06e5770900a675cede6
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-07-29 11:22:14 -0300

    Resolviendo ejercicio 7

--- ejercicio-7.js
+++ ejercicio-7.js
@@ -2,13 +2,13 @@
 Crea un programa que liste todos los archivos en un directorio específico. */
 const fs = require("node:fs");
 
-console.log(__dirname);
+//console.log(__dirname);
 
 
-fs.readFile(("Ejercicios NodeJS"), (err) => {
+fs.readFile("ejercicio-7.js", {encoding: "utf-8"}, (err, info) => {
     if(err){
         throw new Error;
     }
-})
-
+    console.log(info);
+});
 
