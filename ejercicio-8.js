65ab522fa88e4cdf20a548f0cb2f17142ef8657d
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-07-29 15:43:45 -0300

    Realizando ejercicio-8

--- ejercicio-8.js
+++ ejercicio-8.js
@@ -3,6 +3,13 @@ Crea una aplicación en NodeJS que utilice el módulo os para proporcionar infor
 sobre el sistema operativo y el entorno en el que se está ejecutando la aplicación. 
 El servidor debe tener dos rutas:
 GET /system-info: Devuelve un JSON con la siguiente información:
+Nombre del sistema operativo
+Versión del sistema operativo
+Arquitectura del procesador
+Memoria total del sistema (en GB)
+Memoria libre del sistema (en GB)
+Número de CPUs disponibles
+Información del hostname (nombre del host)
 GET /uptime: Devuelve un JSON con el tiempo de actividad del sistema (en segundos) 
 desde que se inició el sistema.
 */
@@ -13,6 +20,19 @@ const os = require("node:os");
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
+    if(req.url === "/system-info"){
+        res.end({
+            name: os.release(),
+            version: os.version(),
+            arch: os.arch(),
+            total_memory: os.totalmem(),
+            free_memory: os.freemem(),
+            cpus_free: os.cpus(),
+            hostname: os.hostname() 
+        });
+    }
+
+    //res.end(info.json());
     
     res.end("Hola Mundo");
 });
