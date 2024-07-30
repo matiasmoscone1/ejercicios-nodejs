669cf9c60abcfbf1cae2ade9b021a03e896ba8d2
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-07-30 11:59:13 -0300

    Agregando check-disk-space ya que diskusage no esta disponible en la version 20.4 de node

--- ejercicio-9.js
+++ ejercicio-9.js
@@ -5,6 +5,7 @@ Crea un script en NodeJS que determine si una PC es veloz y si se puede mejorar
 
 const os = require("node:os");
 const http = require("node:http");
+const checkDiskSpace = require("check-disk-space").default;
 
 
 const port = process.env.PORT || 3000;
@@ -12,15 +13,27 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     if(req.url === "/"){
-        const info = {
-            total_memory: os.totalmem(),
-            cpus: os.cpus(),
-            uptime: os.uptime(),
-            loadvg: os.loadavg(),
-            network_interference: os.networkInterfaces()
-        }        
-        res.end(JSON.stringify(info));
+        checkDiskSpace("C:").then((diskSpace) => {
+            const info = {
+                total_memory: os.totalmem(),
+                cpus: os.cpus(),
+                uptime: os.uptime(),
+                loadvg: os.loadavg(),
+                network_interference: os.networkInterfaces(),
+                disk_space: {
+                    total: diskSpace.size,
+                    free: diskSpace.free,
+                    used: diskSpace.size - diskSpace.free
+                }
+            }       
+            res.end(JSON.stringify(info)); 
+        })
+        /* 
+        BLOQUE COMPARATIVO
+        */    
+    
     }
+    
 
 });
 
