469aaa76a194a2f168bbf787ff218e8ba8f1c90e
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-03 20:32:17 -0300

    Probando otra url

--- ejercicio-10-url.js
+++ ejercicio-10-url.js
@@ -3,7 +3,7 @@ Imprime cada uno de estos componentes. https://nodejs.org/api/url.html */
 
 //const url = require("node:url"); -> obsoleto, ahora se usa el constructor global de node URL
 
-const my_url = new URL("https://nodejs.org/api/url.html");
+const my_url = new URL("https://example.com:8080/search?query=nodejs&sort=asc&page=2&filter=active");
 
 console.log(my_url);
 
