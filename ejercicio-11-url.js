e48eb7b7f221a7183152273dd0d2b06839c45f56
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-05 16:35:02 -0300

    Creando objeto para construir la URL

---
+++ ejercicio-11-url.js
@@ -0,0 +1,9 @@
+/* Ejercicio 2: Crea una URL a partir de un objeto que contenga el protocolo, host, path y 
+parámetros de consulta. Asegúrate de que la URL generada sea válida. */
+
+const objectURL = {
+    protocol: "https:",
+    host: "asd"
+}
+
+
