b911953e00da692e6f33b7c37b79d56ebb40072a
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-06 15:13:36 -0300

    Realizando ejercicio-13

---
+++ ejercicio-13-url.js
@@ -0,0 +1,14 @@
+/* Ejercicio 4: Modifica una URL dada para cambiar su path y agregar un nuevo parámetro 
+de consulta. Por ejemplo, cambia el path de http://example.com/oldpath a /newpath 
+y agrega el parámetro foo=bar. */
+
+
+const my_url = new URL("http://example.com/oldpath");
+
+//console.log(my_url);
+
+my_url.pathname = "/newpath";
+
+console.log(my_url.searchParams);
+
+
