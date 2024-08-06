488e9e1137fe9fdcaabc4aa3e55360dbcd7764fc
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-06 15:21:27 -0300

    Mejorando ejercicio 13

--- ejercicio-13-url.js
+++ ejercicio-13-url.js
@@ -2,14 +2,24 @@
 de consulta. Por ejemplo, cambia el path de http://example.com/oldpath a /newpath 
 y agrega el parámetro foo=bar. */
 
-
+const rl = require("node:readline");
 const my_url = new URL("http://example.com/oldpath");
 
-//console.log(my_url);
+const interface = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+interface.question("Agrega la propiedad: ", (key) => {
+    interface.question("Agrega el valor: ", (value) => {
+        my_url.searchParams.append(key, value);
+    });
+});
 
 my_url.pathname = "/newpath";
-my_url.searchParams.append("foo", "bar");
+//my_url.searchParams.append("foo", "bar");
 
-console.log(my_url);
+//console.log(my_url);
 
 
