3d58562ce6bc058669b1571820fe8055f14caf5d
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-07 14:12:54 -0300

    Resolviendo ejercicio 18

--- ejercicio-18-url.js
+++ ejercicio-18-url.js
@@ -5,14 +5,13 @@ con los componentes protocol, host, pathname, y search de la URL. */
 
 const my_url = new URL("https://example.com:8080/search?query=nodejs&sort=asc&page=2&filter=active#section");
 
-const my_object = new Object;
 
-//console.log(my_url);
-
-const getObject = () => {
-
+const getObject = (url) => {
+    const my_object = new Object;
     
+
+    console.log(my_url);
 
 }
 
-getObject();
+getObject(my_url);
