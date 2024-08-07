50f63e5df3d952d357bcfef557e2ded5700b70a7
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-07 14:29:21 -0300

    Resolviendo ejercicio 18 con promesas

--- ejercicio-18-url.js
+++ ejercicio-18-url.js
@@ -5,13 +5,28 @@ con los componentes protocol, host, pathname, y search de la URL. */
 
 const my_url = new URL("https://example.com:8080/search?query=nodejs&sort=asc&page=2&filter=active#section");
 
+const my_object = new Object;
 
 const getObject = (url) => {
-    const my_object = new Object;
-    
+    return new Promise((resolve) => {
+        const arrProperty = ["protocol", "host", "pathname", "search"];
 
-    console.log(my_url);
+        arrProperty.forEach((prop) => {
+            my_object[prop] = url[prop];
+        });
+        resolve();
+    });
+    
+    //return(my_object);
 
 }
 
-getObject(my_url);
+
+getObject(my_url).then(() => console.log(my_object));
+
+
+//console.log(getObject(my_url));
+
+
+//console.log(my_url["host"]);
+
