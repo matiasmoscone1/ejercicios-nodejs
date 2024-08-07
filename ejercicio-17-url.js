0a82b20c49e151a21e6490a0ef35b73f93af3834
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-07 14:00:57 -0300

    Realizando ejercicio 18

--- ejercicio-17-url.js
+++ ejercicio-17-url.js
@@ -6,8 +6,15 @@ const my_url = new URL("http://example.com/path");
 
 const my_object = {foo: "bar", tema: "obscuro"};
 
-for(let key in my_object){
+/*for(let key in my_object){
     my_url.searchParams.append(key, my_object[key]);
-}
+}*/
+
+//Object.keys(objeto) -> retorna un array con las propiedades del objeto
+Object.keys(my_object).forEach((key) => {
+    my_url.searchParams.append(key, my_object[key]);
+});
+
 
 console.log(my_url.href);
+
---
+++ ejercicio-18-url.js
@@ -0,0 +1,18 @@
+/* Ejercicio 9: Implementa una función que tome una URL completa y devuelva un objeto 
+con los componentes protocol, host, pathname, y search de la URL. */
+
+// https://example.com:8080/search?query=nodejs&sort=asc&page=2&filter=active#section
+
+const my_url = new URL("https://example.com:8080/search?query=nodejs&sort=asc&page=2&filter=active#section");
+
+const my_object = new Object;
+
+//console.log(my_url);
+
+const getObject = () => {
+
+    
+
+}
+
+getObject();
