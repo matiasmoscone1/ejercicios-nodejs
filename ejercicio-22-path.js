e0b02693ba66398ba6c3a5e1b0b94fc31fdfdaf1
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-09 09:27:52 -0300

    Resolviendo ejercicio 22

--- ejercicio-22-path.js
+++ ejercicio-22-path.js
@@ -1,4 +1,22 @@
 /* Ruta absoluta: Dado un directorio y un archivo, utiliza path.resolve() para generar la 
 ruta absoluta hacia ese archivo. */
 
+const path = require("node:path");
+const fs = require("node:fs");
+
+const dir = "C:/Users/matia/OneDrive/Desktop/Node JS/Ejercicios NodeJS";
+const file = "loadvg.txt";
+
+const completePath = path.resolve(dir, file);
+console.log(completePath);
+
+fs.readFile(file, {encoding: "utf-8"}, (err, info) => {
+    if(err){
+        console.log(err);
+    }else{
+        console.log(info);
+    }
+});
+
+
 
