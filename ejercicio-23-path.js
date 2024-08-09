aaf2fb1b9f6b280932aaa8c53b0512773d986273
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-09 10:15:02 -0300

    ejercicio 23 terminado

--- ejercicio-23-path.js
+++ ejercicio-23-path.js
@@ -4,6 +4,10 @@ path.normalize() para convertirla en una ruta más simple y sin redundancias. */
 const path = require("node:path");
 
 const my_path = "C:\matia/user//docs/./../documents/./project/../files//test.txt";
+const my_path2 = "C:\matia/user/documents/home../music/files/californication.mp3";
 
-console.log(path.normalize(my_path));
+//console.log(path.normalize(my_path));
 
+const relativePath = path.relative(path.normalize(my_path), path.normalize(my_path2));
+
+console.log(relativePath);
