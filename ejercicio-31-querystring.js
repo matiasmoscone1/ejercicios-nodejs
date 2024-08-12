7fdc3388991950826325e175a74e84d81fbdf6c6
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-12 16:29:35 -0300

    Mejorando ejercicio 31

--- ejercicio-31-querystring.js
+++ ejercicio-31-querystring.js
@@ -5,22 +5,21 @@ querystring.stringify() para agregar nuevos parámetros sin sobrescribir los exi
 
 const querystring = require("node:querystring");
 
+//const myURL = new URL("https://example.com/search?category=books&sort=asc&page=2");
 
-const myURL = new URL("https://example.com/search?category=books&sort=asc&page=2");
-/*
-//console.log(myURL.searchParams.append("color", "red"));
+const parsedQuery = querystring.parse("https://example.com/search?category=books&sort=asc&page=2".split("?")[1]);
 
-myURL.searchParams.append("color", "red")
-*/
-//console.log(myURL);
+parsedQuery.color = "red";
+parsedQuery.address = "Montevideo 323";
 
-const newParams = querystring.stringify({color: "red", adress: "Montevideo 493"});
+//console.log(parsedQuery);
 
-console.log(myURL.href+newParams);
+const constructMyURL2 = querystring.stringify(parsedQuery);
 
-//console.log(querystring.parse("https://example.com/search?category=books&sort=asc&page=2"));
+const constructMyURL = "https://example.com/search?category=books&sort=asc&page=2".split("?")[0];
 
+const myURL = new URL(constructMyURL+"?"+constructMyURL2);
 
-
-
-
+console.log(constructMyURL);
+console.log(constructMyURL2);
+console.log(myURL);
