0b49476a37b1b8be256ee34cf327ad1dd0cae65b
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-13 15:36:31 -0300

    Relizando ejercicio 37

--- ejercicio-37-crypto.js
+++ ejercicio-37-crypto.js
@@ -2,12 +2,15 @@
 su versión hasheada, y verifique si coinciden utilizando el algoritmo SHA-512. */
 
 const crypto = require("node:crypto");
+const readline = require("node:readline");
+//const password = "hola123";
+//const hashedPass = crypto.createHash("sha512").update(password).digest("hex");
+const dataBase = {
+    pass: "31583cff658e71e493ba40d3161b492d1fb10bc70c4faa8c498c7f356c7418fbbbd630a0771a3a71f873c8b1b291bee93e32ddff6bb57c1ca77e2945b6fc0531",
+}
 
-const password = "hola123";
 
-const hashedPass = crypto.createHash("sha256").update(password).digest("hex");
 
-console.log(hashedPass);
 
 
 
