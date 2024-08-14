3183ee2774499aca6c8b8a1b0ce55eb3ec4d03f7
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-14 10:17:38 -0300

    resolviendo ejercicio 38

--- ejercicio-37-crypto.js
+++ ejercicio-37-crypto.js
@@ -29,3 +29,4 @@ interface.question("Ingrese la contraseña: ", (answer) => {
 
 
 
+
---
+++ ejercicio-38-crypto.js
@@ -0,0 +1,22 @@
+/* Encriptar y desencriptar un mensaje con AES: Utiliza el algoritmo de encriptación 
+simétrica AES-256-CBC para encriptar un mensaje. Luego, escribe otro script que 
+desencripte el mensaje cifrado y recupere el texto original. */
+
+const crypto = require("node:crypto");
+
+const secretKey = "asdasdasd";
+const iv = crypto.randomBytes(16);
+
+const cipher = crypto.createCipheriv("aes-256-cbc", secretKey, iv);
+
+const password = "hola123";
+
+const encryptMessage = (msj) => {
+    let encrypted = cipher.update(msj, "utf-8", "hex");
+
+    encrypted += cipher.final("hex");
+    
+    console.log(encrypted);
+}
+
+encryptMessage(password);
