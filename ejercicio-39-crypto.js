78f2a8fd942403298bef40f21545ff3518466bf0
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-14 11:17:39 -0300

    Resolviendo ejercicio 39

---
+++ ejercicio-39-crypto.js
@@ -0,0 +1,34 @@
+/* Generar un par de claves pública y privada RSA: Genera un par de claves RSA 
+(pública y privada) utilizando crypto.generateKeyPairSync() y guárdalas en 
+archivos separados. */
+
+const crypto = require("node:crypto");
+const fs = require("node:fs");
+
+const keys = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+        type: "spki",
+        format: "pem"
+    },
+    privateKeyEncoding: {
+        type: "pkcs8",
+        format: "pem"
+    }
+});
+
+console.log(keys.publicKey, keys.privateKey);
+
+fs.writeFile("publicKey.txt", keys.publicKey, (err) => {
+    if(err){
+        console.log("Ocurrio un error al crear el archivo...", err);
+    }
+});
+fs.writeFile("privateKey.txt", keys.privateKey, (err) => {
+    if(err){
+        console.log("Ocurrio un error al crear el archivo...", err);
+    }
+});
+
+
+
