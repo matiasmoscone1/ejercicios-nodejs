032fdc199a6e81b5815c365da219af8f47a042d5
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-15 14:34:47 -0300

    Mejorando ejercicio 43

--- ejercicio-43-crypto.js
+++ ejercicio-43-crypto.js
@@ -2,6 +2,8 @@
 enunciado anterior para encriptar y luego desencriptar un archivo de texto. */
 
 const crypto = require("node:crypto");
+const fs = require("node:fs");
+const path = require("node:path");
 
 const password = "hola123";
 
@@ -11,14 +13,14 @@ const devKey = () => {
             if(err){
                 throw reject;
             }else{
-                resolve(derivedKey.toString("hex"));
+                resolve(derivedKey);
                 //console.log(derivedKey.toString("hex"));
             }
         });   
     });
 }
 
-
+/*
 devKey().then((key) => {
     const secretKey = crypto.randomBytes(32);
     const iv = crypto.randomBytes(16);
@@ -34,5 +36,27 @@ devKey().then((key) => {
     console.log(encrypted);
     console.log(decrypt);
 });
+*/
+devKey().then((key) => {
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
+    const decipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
+    
+    const input = fs.createReadStream(path.join(__dirname, "mensaje.txt"));
+    const output = fs.createWriteStream(path.join(__dirname, "mensaje_encriptado.txt"));
+    
+    input.on("data", (chunk) => {
+        const encryptedChunk = cipher.update(chunk);
+        output.write(encryptedChunk);
+    });
+    
+    output.on("end", () => {
+        const finalChunk = cipher.final();
+        output.write(finalChunk);
+        console.log("Archivo encriptado");    
+    })
+    
+});
+
 
 
