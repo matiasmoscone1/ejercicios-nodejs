b8aff5b65db3bdff8c32398ed5fc675f98653e48
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-15 13:44:29 -0300

    Agregando promesa a ejercicio 43

--- ejercicio-43-crypto.js
+++ ejercicio-43-crypto.js
@@ -3,8 +3,23 @@ enunciado anterior para encriptar y luego desencriptar un archivo de texto. */
 
 const crypto = require("node:crypto");
 
+const password = "hola123";
 
+const devKey = () => {
+    return new Promise((resolve, reject) => {
+        crypto.pbkdf2(password, "salt", 100000, 32, "sha512", (err, derivedKey) => {
+            if(err){
+                throw reject;
+            }else{
+                resolve(derivedKey.toString("hex"));
+                //console.log(derivedKey.toString("hex"));
+            }
+        });   
+    });
+}
 
-
+devKey().then((key) => {
+    console.log(key);
+})
 
 
