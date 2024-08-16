b563ad11a70192ff751856f32a45bf12ab3fa557
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-16 15:08:37 -0300

    Mejorando ejercicio 45

--- ejercicio-45-crypto.js
+++ ejercicio-45-crypto.js
@@ -3,14 +3,29 @@ utilizando SHA-256 para un mensaje dado, y luego escribe un script que verifique
 integridad del mensaje usando el HMAC. */
 
 const crypto = require("node:crypto");
+const rl = require("node:readline");
 
 const message = "Hola muy buenos dias soy matias";
 const key = "asdasd";
 
 const hmac = crypto.createHmac("sha256", key).update(message).digest("hex");
 
+const interface = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-console.log(hmac);
+interface.question("Escribe el mensaje: ", (answer) => {
+    const hmac2 = crypto.createHmac("sha256", key).update(answer).digest("hex");
+    if(hmac === hmac2){
+        console.log("Mensaje valido, no ha sido modificado");
+    }else{
+        console.log("Mensaje invalido, ha sido modificado...");
+    }
+    interface.close();
+});
+
+//console.log(hmac);
 
 
 
