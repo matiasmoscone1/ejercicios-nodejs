9398ddd3d868021968328f77505a054339a581bf
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-17 14:36:13 -0300

    Resolviendo ejercicio 46

---
+++ ejercicio-46-events.js
@@ -0,0 +1,18 @@
+/* Emisión y Escucha Básica: Crea un EventEmitter, emite un evento llamado saludo y 
+escucha ese evento para imprimir un mensaje en la consola. */
+
+const events = require("node:events");
+
+const myEmitter = new events.EventEmitter();
+
+myEmitter.on("saludo", () => {
+    console.log("Hola estoy saludando!!!");
+});
+
+myEmitter.on("saludo", () => {
+    console.log("Hola estoy saludando nuevamente!!!!");
+});
+
+
+myEmitter.emit("saludo");
+
