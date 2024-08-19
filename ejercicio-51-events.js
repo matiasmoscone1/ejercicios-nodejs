1764904c14b4a94251892df95af3cac33f93252e
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-19 15:38:51 -0300

    Mejorando ejercicio 51

---
+++ ejercicio-51-events.js
@@ -0,0 +1,21 @@
+/* Encadenamiento de Listeners: Emite un evento llamado procesar que pase por tres 
+listeners diferentes, cada uno realizando una operación sobre un valor inicial. 
+El primer listener multiplica el valor por 2, el segundo le suma 10, y el tercero 
+imprime el resultado final. */
+
+const events = require("node:events");
+
+const myEmitter = new events.EventEmitter();
+
+let value = 10;
+
+myEmitter.on("procesar", () => {
+    value = value * 2;
+}).on("procesar", () => {
+    value = value + 10;
+}).on("procesar", () => {
+    console.log("El resultado es: ", value);
+});
+ 
+
+myEmitter.emit("procesar");
