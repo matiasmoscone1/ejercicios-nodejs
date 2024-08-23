f4ced027c7cd2a680758692bfa6a0aac5d420f18
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-23 17:15:46 -0300

    Resolviendo ejercicio 59

--- ejercicio-59-stream.js
+++ ejercicio-59-stream.js
@@ -1,5 +1,13 @@
 /* Lo mismo que el ejercicio 58 pero ahora para descomprimir el archivo. */
 
+const fs = require("node:fs");
+const zlib = require("node:zlib");
+
+const readableStream = fs.createReadStream("mensaje_comprimido.txt.gz");
+const writableStream = fs.createWriteStream("mensaje_descomprimido.txt");
+const descomprimeStream = zlib.createGunzip();
+
+readableStream.pipe(descomprimeStream).pipe(writableStream);
 
 
 
