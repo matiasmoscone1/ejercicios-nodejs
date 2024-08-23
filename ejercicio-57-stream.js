ed0c3e9f894fdae77837212d97f12f19a75e19cc
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-23 16:52:17 -0300

    Ejercicio 57 resuelto correctamente

--- ejercicio-57-stream.js
+++ ejercicio-57-stream.js
@@ -4,6 +4,9 @@ convierta todo el contenido a mayúsculas antes de escribirlo en un nuevo archiv
 const stream = require("node:stream");
 const fs = require("node:fs");
 
+const readableStream = fs.createReadStream("mensaje.txt");
+const writableStream = fs.createWriteStream("mensaje_transform.txt");
+
 const tra = new stream.Transform({
     transform(chunk, encoding, callback){
         const upperCaseChunk = chunk.toString().toUpperCase();
@@ -12,5 +15,18 @@ const tra = new stream.Transform({
     }
 });
 
-console.log(tra);
+readableStream.pipe(tra).pipe(writableStream);
+
+readableStream.on("error", (err) => {
+    console.log("Error al leer el archivo: ", err);
+});
+
+writableStream.on("error", (err) => {
+    console.log("Error al escribir el archivo: ", err);
+});
+
+tra.on("error", (err) => {
+    console.log("Error al transformar el archivo: ", err);
+});
+
 
