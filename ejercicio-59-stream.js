1d593927ecce06a82048ffbe2e117a80852fc120
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-23 17:20:37 -0300

    Mejorando ejercicio 59

--- ejercicio-59-stream.js
+++ ejercicio-59-stream.js
@@ -9,6 +9,15 @@ const descomprimeStream = zlib.createGunzip();
 
 readableStream.pipe(descomprimeStream).pipe(writableStream);
 
+readableStream.on("error", (err) => {
+    console.log("Error al leer el archivo: ", err);
+});
 
+writableStream.on("error", (err) => {
+    console.log("Error al escribir el archivo: ", err);
+});
 
+descomprimeStream.on("error", (err) => {
+    console.log("Error al descomprimir el archivo: ", err);
+});
 
