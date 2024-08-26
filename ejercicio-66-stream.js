052cf6174f713d5290cc1bbe5f1af4afe5e76334
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-26 16:38:28 -0300

    Mejorando ejercicio 66

---
+++ ejercicio-66-stream.js
@@ -0,0 +1,36 @@
+/* Implementación de un Duplex stream: Implementa un Duplex stream personalizado que actúe 
+como un eco, es decir, que cualquier dato escrito en él sea inmediatamente leído de nuevo. */
+
+const stream = require("node:stream");
+
+const duplex = new stream.Duplex({
+    read(size){
+
+    },
+    write(chunk, encoding, callback){
+        this.push(chunk);
+        callback();
+    },
+    final(callback){
+        console.log("Stream finalizado...");
+        callback();
+    }
+});
+
+duplex.write(`Hola soy Matias! \n`);
+duplex.write(`Hola esto es un Eco. \n`);
+
+duplex.on("data", (chunk) => {
+    console.log(`Datos leeidos: ${chunk.toString()}`);
+})
+
+duplex.end();
+
+
+
+
+
+
+
+
+
