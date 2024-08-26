b7c0e58f19676f3df7912c0bf76dce4be4554eeb
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-26 14:55:48 -0300

    Creando JSON para ejercicio 63

---
+++ ejercicio-63-stream.js
@@ -0,0 +1,11 @@
+/* Filtrado de datos: Crea un Transform stream que filtre un archivo JSON de manera que solo 
+los objetos que cumplan con ciertos criterios (por ejemplo, edad > 30) sean escritos en 
+un nuevo archivo JSON. */
+
+
+// guia ejercicio 57
+
+
+
+
+
