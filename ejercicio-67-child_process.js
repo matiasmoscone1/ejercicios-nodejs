2ba9c8212aeb8633fba43671a6fb7142fc31aebf
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-27 16:34:03 -0300

    Ejercicio 67 terminado

--- ejercicio-67-child_process.js
+++ ejercicio-67-child_process.js
@@ -1,13 +1,21 @@
 /* Crear un proceso hijo que ejecute un comando de shell básico: Usa child_process.exec 
-para ejecutar ls -la y muestra el resultado en la consola. */
+para ejecutar dir y muestra el resultado en la consola. */
 
 const child_process = require("node:child_process");
 
 
-child_process.exec("ls -la", (err) => {
+child_process.exec("dir", (err, stdout, stderr) => {
     if(err){
         console.log("Error al ejecutar el comando: ", err);
+        return;
     }
+    if(stderr){
+        console.log("Error en el comando: ", stderr);
+        return;
+    }
+
+    console.log("Informacion del comando: ", stdout);
+
 });
 
 
