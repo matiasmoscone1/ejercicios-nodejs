45cadb01525927a75d9f3b449b4f17e36461217c
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-28 14:34:54 -0300

    Resolviendo ejercicio 74

---
+++ ejercicio-74-child_process.js
@@ -0,0 +1,18 @@
+/* Gestionar la salida estándar y la salida de errores de un proceso hijo por separado: 
+Usa child_process.spawn para ejecutar un comando y captura tanto stdout como stderr 
+por separado. */
+
+const child_process = require("node:child_process");
+
+const command = "ipconfig";
+const child = child_process.spawn(command);
+
+child.stdout.on("data", (data) => {
+    console.log(`Resuldao del comando ${command}: ${data}`);
+});
+
+child.stderr.on("error", (err) => {
+    console.log(`Error al ejecutar el comando ${command}: ${err}`);
+});
+
+
