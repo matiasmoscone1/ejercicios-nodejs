3b1f22654fb0f7ecd13538ee96fa54295417ab04
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-28 15:10:15 -0300

    Resolviendo ejercicio 76

---
+++ ejercicio-76-child_process.js
@@ -0,0 +1,22 @@
+/* Crear un proceso hijo y finalizarlo de manera programática: Usa child_process.spawn 
+para iniciar un proceso y luego termina el proceso hijo usando process.kill. */
+
+const child_process = require("node:child_process");
+
+const child = child_process.spawn("ping", ["google.com"]);
+
+
+child.stdout.on("data", (data) => {
+    console.log("Resultado: ", data.toString());
+});
+
+child.stderr.on("data", (data) => {
+    console.log("Error en la ejecucion del proceso: ", data);
+});
+
+child.on("error", (err) => {
+    console.log("Ha ocurrido un error: ", err);
+});
+
+
+
