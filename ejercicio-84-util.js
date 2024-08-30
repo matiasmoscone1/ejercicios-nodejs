bd9296d2551234b0d47efb84c523f43b46505519
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-30 09:21:27 -0300

    Ejercicio 84 resuelto

--- ejercicio-84-util.js
+++ ejercicio-84-util.js
@@ -1,13 +1,21 @@
 /* util.TextEncoder y util.TextDecoder: Convierte una cadena de texto a un Uint8Array 
 usando TextEncoder y luego conviértelo de vuelta a una cadena de texto utilizando TextDecoder. */
 
+const { decode } = require("node:punycode");
 const util = require("node:util");
 
 const stringConver = "Hi my name is Matias and i have 28 years old";
 
 const encoder = new util.TextEncoder();
 
-console.log(encoder.encode(stringConver));
+const conversedString = encoder.encode(stringConver);
 
+console.log(`String convertido: ${conversedString}`);
+
+const decoder = new util.TextDecoder();
+
+const decodedString = decoder.decode(conversedString);
+
+console.log(`String deconvertido: ${decodedString}`);
 
 
