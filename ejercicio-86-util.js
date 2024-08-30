481b79ee6acd4b45065a6f977d929bbeb2b23aec
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-08-30 10:17:40 -0300

    Resolviendo ejercicio 86

---
+++ ejercicio-86-util.js
@@ -0,0 +1,14 @@
+/* util.log: Escribe una función que registre un mensaje en la consola utilizando util.log, 
+que incluye una marca de tiempo junto con el mensaje. */
+
+const util = require("node:util");
+
+const functionMessage = () => {
+
+    util.log("Probando util.log");
+
+}
+
+functionMessage();
+
+
