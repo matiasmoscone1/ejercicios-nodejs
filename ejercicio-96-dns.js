6cea6d7f655b29af077a726869a6e62c176ccfbc
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-01 13:16:34 -0300

    Mejorando ejercicio 96

--- ejercicio-96-dns.js
+++ ejercicio-96-dns.js
@@ -8,6 +8,15 @@ const dns = require("node:dns");
 const servers = dns.getServers();
 
 console.log(servers);
+dns.resolve("google.com", (err, data) => {
+    if(err){
+        console.log("Ha ocurrido un error al obtener la ip:", err);
+    }else{
+        console.log("Ip:", data);
+    }
+    dns.setServers(data);
+});
+
 
 setTimeout(() => {
     console.log(servers);
