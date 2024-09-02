9012fe8d04463ebbc2e4a01c25772b3a9629f345
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-02 04:32:34 -0300

    Mejorando ejercicio 99

--- ejercicio-99-https.js
+++ ejercicio-99-https.js
@@ -3,17 +3,31 @@ Crea un servidor HTTPS que sirva un archivo HTML estático desde el sistema de a
 cuando un cliente acceda al servidor. */
 
 const https = require("node:https");
+const fs = require("node:fs");
 
 const server = https.createServer((req, res) => {
+ 
+    if(req.url === "/archivo"){
+        const readableFile = fs.createReadStream("archivo-index.html");
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        readableFile.pipe(res);
 
-    res.writeHead(200, {});
-    res.end("Mi servidor funcionando...");
+        readableFile.on("error", (err) => {
+            res.writeHead(500);
+            res.end("Error al cargar el archivo...");
+            console.log("Error:", err);
+        });
+    }else{
+        res.end("Recurso no encontrado...");
+    }
 
 });
 
 server.listen(3000, (err) => {
     if(err){
         console.log("Ha ocurrido un error en el servidor:", err);
+    }else{
+        console.log("Servidor levantado en el puerto: 3000");
     }
 });
 
