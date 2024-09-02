3227268ec363d6d3d90c2922c3214913a8038fd8
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-02 05:14:30 -0300

    Terminando ejercicio 99, creando certificado autofirmado no oficial con selfsigned

--- ejercicio-99-https.js
+++ ejercicio-99-https.js
@@ -4,12 +4,21 @@ cuando un cliente acceda al servidor. */
 
 const https = require("node:https");
 const fs = require("node:fs");
+const selfsigned = require("selfsigned");
 
-const server = https.createServer((req, res) => {
+const attrs = [{ name: "commonName", value: "localhost" }];
+const pems = selfsigned.generate(attrs, { days: 720 });
+
+const options = {
+    key: pems.private,
+    cert: pems.cert
+}
+
+const server = https.createServer(options, (req, res) => {
  
     if(req.url === "/archivo"){
         const readableFile = fs.createReadStream("archivo-index.html");
-        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.writeHead(200, {"Content-Type": "text/html"});
         readableFile.pipe(res);
 
         readableFile.on("error", (err) => {
@@ -18,6 +27,7 @@ const server = https.createServer((req, res) => {
             console.log("Error:", err);
         });
     }else{
+        res.writeHead(404, { "Content-Type": "text/plain" });
         res.end("Recurso no encontrado...");
     }
 
