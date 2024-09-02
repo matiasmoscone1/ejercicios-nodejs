348309338a80a035f00fe965e012b96d576a2216
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-02 14:55:34 -0300

    Resolviendo ejercicio 100

--- ejercicio-100-https.js
+++ ejercicio-100-https.js
@@ -4,8 +4,10 @@ equivalente HTTPS. */
 
 const https = require("node:https");
 const http = require("node:http");
+const selfsigned = require("selfsigned");
 
 const port = process.env.PORT || 3000;
+const portHTTPS = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
 
@@ -27,4 +29,31 @@ server.listen(port, (err) => {
     }
 });
 
+const attrs = [{ name: "commonName", value: "localhost" }];
+const pems = selfsigned.generate(attrs, { days: 720 });
+
+const options = {
+    key: pems.private,
+    cert: pems.cert
+}
+
+const serverHTTPS = https.createServer(options, (req, res) => {
+    
+    if(req.url === "/"){
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end("Hola bienvenido al servidor https");
+    }else{
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.end("Ha ocurrido un error...");
+    }
+
+});
+
+serverHTTPS.listen(portHTTPS, (err) => {
+    if(err){
+        console.log("Ha ocurrido un error:", err);
+    }else{
+        console.log("Servidor https escuchando en el puerto:", portHTTPS);
+    }
+});
 
