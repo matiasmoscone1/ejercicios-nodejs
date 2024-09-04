6ad16e3c91c5e09808f77509469cf7c860ad7ff3
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-04 14:30:39 -0300

    Mejorando ejercicio 103

--- ejercicio-103-https.js
+++ ejercicio-103-https.js
@@ -22,20 +22,47 @@ const options = {
 };
 
 const req = https.request(options, (res) => {
+   
     res.on("data", (data) => {
         console.log(data.toString());
     });
  
     res.on("error", (err) => {
         console.log("Ha ocurrido un error al obtener los datos:", err);
-    })
+    });
 
 });
 
 req.end();
 
+const options2 = {
+    hostname: "jsonplaceholder.typicode.com",
+    port: 443,
+    path: "/users",
+    headers: {"Content-Type": "application/json"},
+    method: "POST"
+}
 
+const info = JSON.stringify({
+    id: 11,
+    name: "Matias",
+    username: "Mos",
+    email: "matiasmos@hotmail.com" 
+});
 
+const req2 = https.request(options2, (res) => {
 
+    res.on("data", (data) => {
+        console.log(data.toString());
+    });
 
+    res.on("error", (err) => {
+        console.log("Ha ocurrido un error al obtener los datos:", err);
+    });
+
+});
+
+req2.write(info);
+
+req2.end();
 
