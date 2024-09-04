5cd69f9074011915cec79b9f196bd6006f5d56ec
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-04 15:13:26 -0300

    Resolviendo ejercicio 104

--- ejercicio-104-https.js
+++ ejercicio-104-https.js
@@ -5,7 +5,7 @@ servidor externo (por ejemplo, manejo de errores de red, tiempos de espera, etc.
 const https = require("node:https");
 
 const options = {
-    hostname: "jsonplaceholder.typicode.com",
+    hostname: "jsonplDFGDFaceholder.typicode.com",
     port: 443,
     path: "/users",
     method: "GET"
@@ -17,9 +17,21 @@ const req = https.request(options, (res) => {
         data += chunk;
         console.log(data.toString());
     });
+    res.on("end", () => {
+        if(res.statusCode >= 200 && res.statusCode < 300){
+            console.log("Datos recibidos con exito:", data.toString());
+        }else{
+            console.log("Ha ocurrido un error:", res.statusCode, res.statusMessage);
+        }
+    });
     res.on("error", (err) => {
-        console.log("Ha ocurrido un error:", err);
-    })
+        console.log("Ha ocurrido un error:", err.message);
+    });
+
+});
+
+req.on("error", (err) => {
+    console.log("Ha ocurrido un error:", err);
 });
 
 req.end();
