66d880634801e47f99cb7bfa12090d7d5588ed4e
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-06 18:12:38 -0300

    Mejorando ejercicio 105

--- ejercicio-105-https.js
+++ ejercicio-105-https.js
@@ -3,23 +3,29 @@ Configura un servidor HTTPS que requiera autenticación básica (usuario y contr
 para acceder a ciertos endpoints. */
 
 const https = require("node:https");
+const selfsigned = require("selfsigned");
+
 
 const port = process.env.PORT || 443;
 
 const usuario = new Buffer("userprueba").toString("base64");
 const contrasenia = new Buffer("passprueba").toString("base64");
 
+const attrs = [{ name: "commonName", value: "localhost" }];
+const pems = selfsigned.generate(attrs, { days: 720 });
+
 
 const options = {
+    key: pems.private,
+    cert: pems.cert,
     hostname: "https://localhost",
     path: "/registro",
     port: 443,
     method: "GET",
-    auth: {
-        user: usuario,
-        pass: contrasenia
-    }    
-}
+    headers: {
+        "Authorization": "Basic" + new Buffer(usuario + ":" + contrasenia).toString("base64")
+    }
+};
 
 const server = https.createServer(options, (req, res) => {
 
