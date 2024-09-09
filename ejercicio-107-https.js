e57b5745ce308169fb6b866b50a4f6bbb19eb3ce
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-09 15:25:16 -0300

    Mejorando ejercicio 107

--- ejercicio-107-https.js
+++ ejercicio-107-https.js
@@ -3,6 +3,9 @@ Crea un cliente HTTPS que se conecte a un servidor utilizando un certificado de 
 personalizado para autenticar la conexión.*/ 
 
 const https = require("node:https");
+const selfsigned = require("selfsigned");
+
+const port = process.env.PORT || 443;
 
 const attrs = [{ name: "commonName", value: "localhost" }];
 const pems = selfsigned.generate(attrs, { days: 720 });
@@ -18,13 +21,19 @@ const options = {
 
 const server = https.createServer(options, (req, res) => {
     if(req.url === "/"){
+        res.writeHead(200, {"Content-Type":"text/plain"});
         res.end("Estas en la pagina principal");
+    }else{
+        res.writeHead(404);
+        res.end("Ha ocurrido un error...");
     }
 });
 
 server.listen(port, (err) => {
     if(err){
         console.log("Ha ocurrido un error en el servidor:", err);
+    }else{
+        console.log("Servidor levantado en el puerto:", port);
     }
 });
 
