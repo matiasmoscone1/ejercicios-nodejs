989574a6a72e4388d4a85294ae55907720cb023e
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-11 16:08:29 -0300

    Creando estructura del ejercicio 110

---
+++ ejercicio-110-express.js
@@ -0,0 +1,13 @@
+/* Manejo de archivos estáticos: Sirve archivos estáticos como HTML, CSS o imágenes desde 
+una carpeta public. */
+
+const express = require("express");
+const app = express();
+
+
+
+
+
+
+
+
