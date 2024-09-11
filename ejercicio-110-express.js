d195896856345907ff50ac5e17878b568988743f
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-11 16:38:56 -0300

    Resolviendo ejercicio 110

--- ejercicio-110-express.js
+++ ejercicio-110-express.js
@@ -3,11 +3,27 @@ una carpeta public. */
 
 const express = require("express");
 const app = express();
+const fs = require("node:fs");
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+
+    const readableFile = fs.createReadStream("public/index.html");
+    const readableFile2 = fs.createReadStream("public/index.css");
+    
+    readableFile2.pipe(res);
+
+});
 
 
-
-
-
+app.listen(port, (err) => {
+    if(err){
+        console.log("Ha ocurrido un error al levantar el servidor:", err);
+    }else{
+        console.log("Servidor levantado en el puerto:", port);
+    }
+});
 
 
 
