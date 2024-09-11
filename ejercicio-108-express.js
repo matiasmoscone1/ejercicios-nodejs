7e0ae9b6e2818b1ea5187cb3b727f58028db6cc3
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-11 15:10:37 -0300

    Mejorando ejercicio 108

--- ejercicio-108-express.js
+++ ejercicio-108-express.js
@@ -1,5 +1,6 @@
 /* Servidor básico con Express: Crea un servidor simple que responda "Hello World" en 
-la ruta raíz (/). */
+la ruta raíz (/). 
+Rutas básicas: Agrega rutas como /about y /contact que respondan con diferentes mensajes. */
 
 const express = require("express");
 const app = express();
@@ -7,7 +8,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Bienvenido, esta es la pagina principal");
+    res.send("Bienvenido a la pagina principal");
+});
+
+app.get("/about", (req, res) => {
+    res.send("Bienvenido a la seccion about");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("Bienvenido a la seccion contact");
 });
 
 app.listen(port, (err) => {
