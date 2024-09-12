eb8e2ed49ced68f6cd84407bd0ef2f52f6042a60
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-12 17:25:01 -0300

    Mejorando ejercicio 112

--- ejercicio-112-express.js
+++ ejercicio-112-express.js
@@ -6,14 +6,26 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+const isLogged = (req) => {
+    return req.query.auth === true;
+}
+
 app.get("/old-page", (req, res) => {
-    res.status(301).redirect("/new-page");
+    if(isLogged){
+        res.status(301).redirect("/new-page");
+    }else{
+        res.status(302).redirect("/login");
+    }
+
 });
 
 app.get("/new-page", (req, res) => {
     res.status(200).send("Se redirigió a la nueva pagina :)");
 });
 
+app.get("/login", (req, res) => {
+    res.send("Porfavor inicie sesion");
+});
 
 app.listen(port, (err) => {
     if(err){
