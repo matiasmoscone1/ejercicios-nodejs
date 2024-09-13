f222db6e258a37824a0dca82a607168bf87557f7
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-13 15:07:08 -0300

    Resolviendo ejercicio 114

---
+++ ejercicio-114-express.js
@@ -0,0 +1,28 @@
+/* Manejo de errores simples: Implementa un middleware de manejo de errores para capturar 
+rutas no encontradas. */
+
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.status(200).send("Bienvenido a la pagina principal");
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Ocurrio un error al cargar la pagina");
+});
+
+app.listen(port, (err) => {
+    if(err){
+        console.log("Ha ocurrido un error al levantar el servidor:", err);
+    }else{
+        console.log("Servidor levantado en el puerto:", port);
+    }
+});
+
+
+
+
