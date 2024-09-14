5490d0bdbc0739a5a165e329a7eab4009ebb0220
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-14 11:51:30 -0300

    Creando estructura del ejercicio 116

--- ejercicio-116-express.js
+++ ejercicio-116-express.js
@@ -1,9 +1,20 @@
 /* Usar next() en middleware: Crea dos middlewares que manejen una solicitud y luego 
 pasen el control con next(). */
 
+const express = require("express");
+const app = express();
 
+const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+    res.status(200).send("Bienvenido a la pagina principal!!!");
+});
 
-
-
+app.listen(port, (err) => {
+    if(err){
+        console.log("Ha ocurrido un error al levantar el servidor:", err);
+    }else{
+        console.log("Servidor levantado en el puerto:", port);
+    }
+});
 
