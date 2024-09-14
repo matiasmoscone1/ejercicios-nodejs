21d1c18528b1ee42b6bcb394ee1bdbde15020288
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-14 10:59:12 -0300

    Mejorando ejercicio 115

--- ejercicio-115-express.js
+++ ejercicio-115-express.js
@@ -6,14 +6,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-
+    res.status(200).send("Bienvenido a la pagina principal");
 });
 
 app.get("/search", (req, res) => {
-
+    res.status(200).send("Bienvenido a la pagina de busqueda :)");
 });
 
-
+app.use((req, res, next) => {
+    console.log(req.url);
+});
 
 
 app.listen(port, (err) => {
