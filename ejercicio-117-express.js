ce0626345d0544aace8ae38ec42a71dc34d44503
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-16 11:10:31 -0300

    Resolviendo ejercicio 117

--- ejercicio-117-express.js
+++ ejercicio-117-express.js
@@ -3,12 +3,17 @@ procese el cuerpo del formulario. */
 
 const express = require("express");
 const app = express();
+const path = require("node:path");
 
 const port = process.env.PORT || 3000;
 
 
 app.get("/", (req, res) => {
     res.status(200).send("Bienvenido a la pagina principal");
+});
+
+app.get("/register", (req, res) => {
+    res.status(200).sendFile(`${path.join(__dirname)}/formulario.html`);
 });
 
 app.listen(port, (err) => {
