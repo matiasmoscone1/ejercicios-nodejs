e64c3898a62a793ea53cc84643831242996adcfa
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-24 11:13:55 -0300

    Resolviendo ejercicio 121

--- ejercicio-121-express.js
+++ ejercicio-121-express.js
@@ -6,6 +6,13 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(cookieParser("123456", {}));
+
+app.get("/", (req, res) => {
+    console.log(req.cookies);
+    res.status(200).send("Bienvenido a la pagina principal");
+});
+
 app.listen(port, (err) => {
     if(err){
         console.log("Ha ocurrido un error al levantar el servidor:", err);
