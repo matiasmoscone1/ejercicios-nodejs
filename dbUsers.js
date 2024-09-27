e97385079c96dd8e44507f3b10cf4e796e2104bb
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-27 08:49:11 -0300

    Estructurando base de datos del ejercicio 124

---
+++ dbUsers.js
@@ -0,0 +1,84 @@
+
+const dbUsers = [{
+    id: 1,
+    nombre: "Matias",
+    Apellido: "Moscone"
+},
+{
+    id: 2,
+    nombre: "Lionel",
+    Apellido: "Messi"
+},
+{
+    id: 3,
+    nombre: "Angel",
+    Apellido: "Di Maria"
+},
+{
+    id: 4,
+    nombre: "Alexis",
+    Apellido: "Mac Allister"
+},
+{
+    id: 5,
+    nombre: "Cristian",
+    Apellido: "Romero"
+},
+{
+    id: 6,
+    nombre: "Nahuel",
+    Apellido: "Molina"
+},
+{
+    id: 7,
+    nombre: "Lisandro",
+    Apellido: "Martinez"
+},
+{
+    id: 8,
+    nombre: "Nicolas",
+    Apellido: "Tagliafico"
+},
+{
+    id: 9,
+    nombre: "Rodrigo",
+    Apellido: "De Paul"
+},
+{
+    id: 10,
+    nombre: "Enzo",
+    Apellido: "Fernandez"
+},
+{
+    id: 11,
+    nombre: "Julian",
+    Apellido: "Alvarez"
+},
+{
+    id: 12,
+    nombre: "Alejandro",
+    Apellido: "Garnacho"
+},
+{
+    id: 13,
+    nombre: "Nicolas",
+    Apellido: "Otamendi"
+},
+{
+    id: 14,
+    nombre: "Leandro",
+    Apellido: "Paredes"
+},
+{
+    id: 15,
+    nombre: "German",
+    Apellido: "Pezzela"
+},
+{
+    id: 16,
+    nombre: "Gonzalo",
+    Apellido: "Montiel"
+}
+];
+
+module.exports = dbUsers;
--- ejercicio-124-express.js
+++ ejercicio-124-express.js
@@ -3,11 +3,14 @@ o productos) en la base de datos. */
 
 const express = require("express");
 const app = express();
+const dbUsers = require("./dbUsers");
 
 const port = process.env.PORT || 3000;
 
-
-
+app.get("/", (req, res) => {
+    console.log(dbUsers);
+    res.status(200).send("Bienvenido a la pagina principal");
+});
 
 app.listen(port, (err) => {
     if(err){
