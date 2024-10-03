21156325ffd270365553561f350dd9661bcf4801
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-10-03 10:10:41 -0300

    Realizando ejercicio 127, practicando express.Router()

---
+++ users.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.status(200).send("Bienvenido a la pagina Users");
+});
+
+router.get("/list", (req, res) => {
+    res.status(200).send("------------- Listado de usuarios -------------");
+});
+
+router.get("/filter", (req, res) => {
+    res.status(200).send("Filtrado de usuarios!!!");
+});
+
+
+
