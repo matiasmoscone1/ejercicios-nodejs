e77b762265f30ca2b2dcf6a439886d1c487219a4
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-10-04 11:52:41 -0300

    estructurando ejercicio 130

---
+++ ejercicio-130-express.js
@@ -0,0 +1,23 @@
+/* 4. Cargar controladores en los routers: Crea un archivo de controladores para cada grupo 
+de rutas (usersController.js, productsController.js, etc.). Los controladores deben manejar 
+la lógica de las respuestas de cada ruta y ser importados en los archivos de rutas que usan 
+express.Router(). Asocia cada ruta en el router con su respectivo controlador. */
+
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const port = process.env.PORT || 4000;
+
+
+
+
+
+app.listen(port, (err) => {
+    if(err){
+        console.log("Ha ocurrido un error al levantar el serivodor:", err);
+    }else{
+        console.log("Se ha levantado el servidor en el puerto:", port);
+    }
+});
+
