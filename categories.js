08b817b62c0c88bacc436f9ffcc62fe867f33b23
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-10-04 08:31:04 -0300

    Terminando ejercicio 128

--- categories.js
+++ categories.js
@@ -6,7 +6,16 @@ router.get("/", (req, res) => {
     res.status(200).json(products);
 });
 
-
+router.get("/:categoryId/products", (req, res) => {
+    try{
+        const data = products.filter((prod) => prod.categoria === req.params.categoryId);    
+        if(data){
+            res.status(200).json(data);
+        }   
+    }catch(err){
+        res.status(500).send("Ha ocurrido un error:", err);
+    }
+});
 
 module.exports = router;
 
