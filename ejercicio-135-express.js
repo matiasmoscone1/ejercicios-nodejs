07e8f6749a7548366f3d1ebf6091a22defd6e610
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-10-10 09:38:03 -0300

    Creando middleware de validacion

--- ejercicio-135-express.js
+++ ejercicio-135-express.js
@@ -27,9 +27,13 @@ app.get("/validation", (req, res) => {
     res.status(200).sendFile(`${__dirname}/formValidation.html`);    
 });
 
-app.post("/validation", (req, res) => {
+app.post("/validation", validationForm, (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
     console.log(req.body);
-
+    res.status(200).send("Datos recibidos correctamente");
 });
 
 app.listen(port, (err) => {
