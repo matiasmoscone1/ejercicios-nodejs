a0d0645eb3c2adda84521ecdbadd81243eb5d395
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-10-10 09:56:27 -0300

    Mejorando manejo de errores del ejercicio 135

--- ejercicio-135-express.js
+++ ejercicio-135-express.js
@@ -27,10 +27,13 @@ app.get("/validation", (req, res) => {
     res.status(200).sendFile(`${__dirname}/formValidation.html`);    
 });
 
-app.post("/validation", validationForm, (req, res) => {
+app.post("/validation", validationForm(), (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        return res.status(400).json({ errors: errors.array() });
+        const errores = errors.array().map((err) => {
+            return({ campo: err.path, mensaje: err.msg});
+        });
+        return(res.status(400).json({ errors: errores }));
     }
     console.log(req.body);
     res.status(200).send("Datos recibidos correctamente");
