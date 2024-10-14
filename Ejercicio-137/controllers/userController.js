3c95395cf4465da5e4948355f628e382a296e6dd
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-10-14 13:19:54 -0300

    chequeando controllers y middlewares

--- Ejercicio-137/controllers/userController.js
+++ Ejercicio-137/controllers/userController.js
@@ -1,10 +1,9 @@
 const User = require("../models/userModel.js");
 const userController = {};
 
-/*
 userController.read = (req, res) => {
-
-}*/
+    res.status(200).send("Aqui sera el endpoint del json de usuarios");
+}
 
 userController.create = (req, res) => {
     const { username, password, rol, email, age } = req.body;
@@ -12,13 +11,13 @@ userController.create = (req, res) => {
     newUser.save();
     res.status(200).send("Usuario creado con exito!");
 }
-/*
+
 userController.update = (req, res) => {
-    
+    res.status(200).send("Aqui se mostrara para cambiar algnu usuario");
 }
 
 userController.delete = (req, res) => {
-    
-}*/
+    res.status(200).send("Aqui sera el delete de usuario");
+}
 
 module.exports = userController;
