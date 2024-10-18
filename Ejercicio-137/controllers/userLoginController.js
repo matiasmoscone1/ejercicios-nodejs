322fe5a2b0002e743fc4c8b8aa5acbc282688e0e
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-10-18 09:07:43 -0300

    Sistema de autenticacion con bcrypt realizado

--- Ejercicio-137/controllers/userLoginController.js
+++ Ejercicio-137/controllers/userLoginController.js
@@ -21,27 +21,21 @@ loginController.login = (req, res) => {
 
 
 loginController.validLogin = async (req, res) => {
-    console.log(req.body);
-
     try{
         const user = await User.findOne({ username: req.body.username });
     
         if(!user){
-            //return {success: false, message: "Usuario no encontrado..."};
             res.status(404).send("Usuario no encontrado...");
         }
 
         const isMatch = await bcrypt.compare(req.body.password, user.password);
     
         if(isMatch){
-            //return {success: true, message: "Autenticacion exitosa!!!"};
             res.status(200).send("Usuario logueado con exito!!!");
         }else{
-            //return {success: false, message: "Contraseña incorrecta..."};
             res.status(404).send("Contraseña incorrecta...");
         } 
     }catch(err){
-        //return {success: false, message: "Error en el servidor..."};
         res.status(500).send("Ha ocurrido un error al autenticar el usuario...");
     }
 
