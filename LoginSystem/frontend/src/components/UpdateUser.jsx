250b01fefae26ca624f1e4a3f2ac1bce2c68d730
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-11-01 08:13:30 -0300

    Creando formulario para fetch basicUpdate

--- LoginSystem/frontend/src/components/UpdateUser.jsx
+++ LoginSystem/frontend/src/components/UpdateUser.jsx
@@ -1,11 +1,30 @@
-
+import { LoginContext } from "../context/LoginContext";
+import { useContext } from "react";
 
 const UpdateUser = () => {
 
+    const { login } = useContext(LoginContext);
+    
+
+    const handleBasicUpdate = async () => {
+        try{
+            const response = await fetch("http://localhost:3000/api/basicUpdate");
+            
+        }
+    }
+
     return(<>
         <div className="update-user-container">
-            <form>
-                
+            <form onSubmit={handleBasicUpdate()}>
+                <label>Username</label>
+                <input type="text" value={login.username} required/>
+                <label>Password</label>
+                <input type="password" value={login.password} required/>
+                <label>Email</label>
+                <input type="email" value={login.email} required/>
+                <label>Age</label>
+                <input type="text" value={login.age} required/>
+                <button>Send</button>
             </form>
         </div>
     </>)
