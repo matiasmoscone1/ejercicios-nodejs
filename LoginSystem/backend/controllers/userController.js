5278106602a5f3cbf98943d89cd57217d183a90f
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-11-01 08:08:17 -0300

    Creando la ruta para la fetch del basicUpdate

--- LoginSystem/backend/controllers/userController.js
+++ LoginSystem/backend/controllers/userController.js
@@ -30,8 +30,10 @@ userController.basicUpdate = async (req, res) => {
         if(user){
             res.status(200).json({message: "Usuario actualizado con exito!"});
         }else{
-            res.status(404).json({message: "Usuario no encontrado..."});
+            res.status(404).json({message: "Usuario no encontrado."});
         }
+    }catch(err){
+        res.status(500).json({message: "No se pudo actualizar el usuario debido a problemas con el servidor."});
     }
 
 }
--- LoginSystem/backend/routes/apiRoutes.js
+++ LoginSystem/backend/routes/apiRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const loginController = require("../controllers/loginController");
+const userController = require("../controllers/userController");
 
 router.post("/auth/login", loginController.auth);
 router.post("/logout", loginController.logout);
 
+router.post("/basicUpdate", userController.basicUpdate);
+router.get("/users",)
 
 module.exports = router;
