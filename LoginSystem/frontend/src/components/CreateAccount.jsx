c2ec38e013782215bb06346bf328dea7964a2349
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-11-07 17:46:08 -0300

    Creando la logica de la creacion de una nueva cuenta

--- LoginSystem/frontend/src/components/CreateAccount.jsx
+++ LoginSystem/frontend/src/components/CreateAccount.jsx
@@ -2,17 +2,41 @@ import { Link } from "react-router-dom";
 
 const CreateAccount = () => {
 
+    const [dataNewUser, setDataNewUser] = useState({});
+
+    const fetchCreateUser = async () => {
+        try{
+            const response = await fetch("http://localhost:3000/api/createUser", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: {
+
+                }
+            })
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();    
+
+    }
+
+    const handleNewUser = (e) => {
+        const {name, value} = e.target;
+        setDataNewUser({...dataNewUser, [name]:value});        
+    }
+
     return(<>
         <div className="create-account-container">
-            <form>
+            <form onSubmit={(e) => handleSubmit(e)}>
                 <label>Username</label>
-                <input type="text" name="username"/>
+                <input type="text" name="username" onChange={(e) => handleNewUser(e)}/>
                 <label>Password</label>
-                <input type="password" name="password"/>
+                <input type="password" name="password" onChange={(e) => handleNewUser(e)}/>
                 <label>Email</label>
-                <input type="email" name="email"/>
+                <input type="email" name="email" onChange={(e) => handleNewUser(e)}/>
                 <label>Age</label>
-                <input type="number" name="age"/>
+                <input type="number" name="age" onChange={(e) => handleNewUser(e)}/>
                 <button className="btn-submit-account" type="submit">Submit</button>
                 <div className="div-account"></div>
                 <Link to={"/"}>
