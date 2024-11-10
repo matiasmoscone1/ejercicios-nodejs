d4a3ccc26466d31ae15a1a44e6ace16bc09ef1ff
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-11-10 00:34:38 -0300

    Debuggeando middleware de update

---
+++ LoginSystem/backend/middlewares/verificationMiddlewareUpdate.js
@@ -0,0 +1,25 @@
+const User = require("../models/userModel");
+
+const verificationMiddlewareUpdate = async (req, res, next) => {
+
+    const {username, email} = req.body;
+    const userId = req.params.id;
+    try{
+        const verifyUser = await User.findOne({ username });
+        if(verifyUser && userId !== verifyUser._id.toString()){
+            return res.status(400).json({message: "El nombre de usuario ya existe."});
+        }
+        const verifyEmail = await User.findOne({ email });
+        if(verifyEmail && userId !== verifyUser._id.toString()){
+            return res.status(400).json({message: "El email ingresado ya existe."});
+        }
+        next();
+    }catch(err){
+        res.status(500).json({message: "Error al verificar usuario y email."});
+    }
+
+}
+
+module.exports = verificationMiddlewareUpdate;
+
+
--- LoginSystem/backend/routes/apiRoutes.js
+++ LoginSystem/backend/routes/apiRoutes.js
@@ -5,6 +5,8 @@ const userController = require("../controllers/userController");
 const adminController = require("../controllers/adminController");
 const adminMiddleware = require("../middlewares/adminMiddleware");
 const verificationMiddleware = require("../middlewares/verificationMiddleware");
+const verificationMiddlewareUpdate = require("../middlewares/verificationMiddlewareUpdate");
+
 
 router.post("/auth/login", loginController.auth);
 router.post("/logout", loginController.logout);
@@ -12,7 +14,7 @@ router.post("/logout", loginController.logout);
 router.get("/adminRead",  adminMiddleware, adminController.read);
 router.delete("/adminDelete/:id", adminMiddleware, adminController.delete);
 router.post("/adminCreate",  adminMiddleware, verificationMiddleware, adminController.create);
-router.post("/adminUpdate/:id", adminMiddleware, verificationMiddleware, adminController.update);
+router.post("/adminUpdate/:id", adminMiddleware, verificationMiddlewareUpdate, adminController.update);
 
 router.post("/createUser", verificationMiddleware, userController.createUser);
 router.post("/basicUpdate", userController.basicUpdate);
