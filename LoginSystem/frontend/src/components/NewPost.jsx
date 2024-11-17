4c1f6581e1966b07320f30735a4e45c448afd791
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-11-16 22:30:58 -0300

    Agregando estado global y contexto a newPost

--- LoginSystem/frontend/src/components/NewPost.jsx
+++ LoginSystem/frontend/src/components/NewPost.jsx
@@ -1,10 +1,14 @@
-
-
+import { useContext } from "react";
+import { LoginContext } from "../context/LoginContext";
 
 const NewPost = () => {
 
+    const { login } = useContext(LoginContext);
+
+
     return(<div className="new-post">
-        <textarea cols={20} rows={5}/>
+        <textarea cols={20} rows={5} name="new-post"/>
+        <button onClick={() => fetchNewPost()}>Post</button>
     </div>)
 
 }
