4c9c8e710e83d22bb5f066113e0c2c9fee03a839
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-11-17 00:08:43 -0300

    Creando fetch newPost

--- LoginSystem/frontend/src/components/NewPost.jsx
+++ LoginSystem/frontend/src/components/NewPost.jsx
@@ -5,9 +5,25 @@ const NewPost = () => {
 
     const { login, dispatchGlobal } = useContext(LoginContext);
 
+    console.log(login);
+    const fetchNewPost = async () => {
+        try{
+            const response = await fetch("http://localhost:3000/api/createPost", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: {
+                    id: login._id,
+                    /*title: , 
+                    content:*/
+                },
+                credentials: "include"
+            })
+        }catch()
+    }
 
     return(<div className="new-post">
-        <textarea cols={20} rows={5} name="new-post"/>
+        <input type="text" name="title-post"/>
+        <textarea cols={20} rows={5} name="content-post"/>
         <div className="btn-new-post">
             <button onClick={() => fetchNewPost()}>Post</button>
             <button onClick={() => {dispatchGlobal({type: "FLAG_NEW_POST", payload: false}); dispatchGlobal({type: "FLAG_BTN_NEW_POST", payload: true})}}>Cancel</button>
