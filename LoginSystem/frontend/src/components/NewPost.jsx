b3c431d7d5a825c8cd050d816d015abe3237f4df
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-11-16 23:05:50 -0300

    mejorando NewPost

--- LoginSystem/frontend/src/components/NewPost.jsx
+++ LoginSystem/frontend/src/components/NewPost.jsx
@@ -3,12 +3,15 @@ import { LoginContext } from "../context/LoginContext";
 
 const NewPost = () => {
 
-    const { login } = useContext(LoginContext);
+    const { login, dispatchGlobal } = useContext(LoginContext);
 
 
     return(<div className="new-post">
         <textarea cols={20} rows={5} name="new-post"/>
-        <button onClick={() => fetchNewPost()}>Post</button>
+        <div className="btn-new-post">
+            <button onClick={() => fetchNewPost()}>Post</button>
+            <button onClick={() => () => dispatchGlobal({type: "FLAG_NEW_POST", payload: false})}>Cancel</button>
+        </div>
     </div>)
 
 }
