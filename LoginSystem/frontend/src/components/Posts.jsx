a1fcbd80f0193dce13d71e24270a7edf54c02d47
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-11-19 14:29:56 -0300

    Acomodando posteos

--- LoginSystem/frontend/src/components/Posts.jsx
+++ LoginSystem/frontend/src/components/Posts.jsx
@@ -8,14 +8,18 @@ const Posts = () => {
     return(<div className="post-container">
         {posts.array.map((post) => {
             return(
-            <div key={post._id}>
-                <div>
-                    <h3>{post.authorUsername}</h3>
-                    <h4>{post.authorRol}</h4>
+            <div key={post._id} className="post-structure">
+                <div className="post-user-rol">
+                    <span>User: <strong>{post.authorUsername}</strong></span>
+                    <span>Rol: <strong>{post.authorRol}</strong></span>
                 </div>
                 <div className="post">
                     <h3>{post.title}</h3>
                     <p>{post.content}</p>
+                </div>
+                <div>
+                    <td className=""><button onClick={() => {}}><i className="fas fa-edit"></i></button></td>
+                    <td className=""><button onClick={() => {}}><i className="fas fa-trash-alt"></i></button></td>
                 </div>
             </div>);
         })}
