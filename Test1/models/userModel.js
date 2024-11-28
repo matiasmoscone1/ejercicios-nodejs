99e7db2fcbe04a103b032eb995b737cc559abc31
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-11-28 14:42:08 -0300

    agregando props para agregar al modelo

---
+++ Test1/models/userModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    lastname: {type: String, required: true},
+    
+})
+
+
+
+
+
