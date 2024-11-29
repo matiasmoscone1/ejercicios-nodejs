45187b3fb6de57527f7f5a5eda820544af697a8d
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-11-29 10:13:02 -0300

    Creando controlador para crear usuarios

--- Test1/controllers/userController.js
+++ Test1/controllers/userController.js
@@ -1,12 +1,23 @@
-const mongoose = require("mongoose");
-
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 const userController = {};
 
 
-userController.read = async () => {
+userController.read = async (req, res) => {
+    try{
+        const users = await User.find();
+        res.status(200).json(users);
+    }catch(err){
+        res.status(404).json({message: "No se encontro ningun usuario."});
+    }
+}
 
+userController.create = async (req, res) => {
     
 
+    try{
+        const user = new User
+    }
 }
 
 
