4f9feed68526c2c0e1e971e59bf6fc1992445574
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-11-29 11:56:50 -0300

    Acomodando rutas y frontend para hacer un fetch de create

--- Test1/backend/app.js
+++ Test1/backend/app.js
@@ -2,9 +2,15 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 require("./config/dbConfig");
+const cors = require("cors");
 const port = process.env.PORT || 4000;
+const api = require("../backend/routes/apiRoutes");
 
+app.use(cors({
+    origin: 'http://localhost:5173', 
+  }));
 
+app.use("/api", api);
 
 app.listen(port, (err) => {
     if(err){
--- Test1/frontend/src/components/CreateUser.jsx
+++ Test1/frontend/src/components/CreateUser.jsx
@@ -1,27 +1,54 @@
-
+import { useState } from "react";
 
 const CreateUser = () => {
 
-    
+    const [data, setData] = useState({
+        username: "",
+        password: "",
+        email: "",
+        firstname: "",
+        lastname: "",
+        role: "",
+        location: "",
+        birthdate: ""});
 
+    const handleInput = (e) => {
+        const {value, name} = e.target;
+        setData({...data, [name]:value});
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
+    const fetchCreate = async () => {
+        try{
+            const response = fetch("http://localhost:3000/api/createUser", 
+            )
+        }
+
+    }
+
+    console.log(data);
     return(<div className="form-container">
-        <form className="form-create-user">
+        <form className="form-create-user" onSubmit={(e) => handleSubmit(e)}>
             <label>Username</label> 
-            <input type="string" name="username" />
+            <input type="string" name="username" onChange={(e) => handleInput(e)}/>
             <label>Password</label> 
-            <input type="password" name="password" />
+            <input type="password" name="password" onChange={(e) => handleInput(e)}/>
             <label>Email</label> 
-            <input type="email" name="email" />
+            <input type="email" name="email" onChange={(e) => handleInput(e)}/>
             <label>Firstname</label> 
-            <input type="string" name="firstname" />
+            <input type="string" name="firstname" onChange={(e) => handleInput(e)}/>
             <label>Lastname</label> 
-            <input type="string" name="lastname" />
+            <input type="string" name="lastname" onChange={(e) => handleInput(e)}/>
             <label>Role</label> 
-            <input type="string" name="role" />
+            <input type="string" name="role" onChange={(e) => handleInput(e)}/>
             <label>Location</label> 
-            <input type="string" name="location" />
+            <input type="string" name="location" onChange={(e) => handleInput(e)}/>
             <label>BirthDate</label> 
-            <input type="date" name="birthdate" />
+            <input type="date" name="birthdate" onChange={(e) => handleInput(e)}/>
             <input type="submit" value="Send" />
         </form>
     </div>)
