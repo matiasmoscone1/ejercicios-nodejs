92bf8e3675bf971f697a592968ff1cf2093d9a1d
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-11-29 10:24:46 -0300

    creando opcion delete user

--- Test1/controllers/userController.js
+++ Test1/controllers/userController.js
@@ -13,14 +13,34 @@ userController.read = async (req, res) => {
 }
 
 userController.create = async (req, res) => {
-    
-
+    const { username, password, email, firstName, lastName, avatar,
+        role, createdAt, location, birthDate } = req.body;
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);    
     try{
-        const user = new User
+        const user = new User({username, hashedPassword, email, firstName, lastName, avatar,
+            role, createdAt, location, birthDate});
+        await user.save();
+        res.status(200).json({message: "Usuario creado con exito!"});
+    }catch(err){
+        res.status(500).json({message: `Ocurrio un error al crear el usuario: ${err.message}`});
     }
 }
 
 
+userController.delete = async (req, res) => {
+    const userId = req.params.id;
+    try{
+        const deletedUser = await User.findByIdAndDelete(userId);
+        if(deletedUser){
+            res.status(200).json({message: "Usuario borrado con exito!"});
+        }else{
+            res.status(404).json({message: "Usuario no encontrado."});
+        }
+    }catch(err){
+        res.status(500).json({message: `Hubo un problema al borrar el usuario: ${err.message}`});
+    }
+}
 
 
 module.exports = userController;
--- Test1/routes/apiRoutes.js
+++ Test1/routes/apiRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.get("/readUser", userController.read);
-router.post("createUser", userController.create);
+router.post("/createUser", userController.create);
+router.delete("/deleteUser", userController.delete);
 
 
 module.exports = router;
