52b6680eaf1c362adf19aa593703d56ad8527c62
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-11-29 09:59:48 -0300

    Creando modelo de usuario

--- Test1/models/userModel.js
+++ Test1/models/userModel.js
@@ -2,12 +2,63 @@ const mongoose = require("mongoose");
 
 
 const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    lastname: {type: String, required: true},
-    
-})
+    username: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 16,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 1024
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 32
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 32
+    },
+    avatar: {
+        type: String,
+    },
+    role: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 16
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    location: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 64
+    },
+    birthDate: {
+        type: Date,
+        required: true,       
+    }
+});
 
+const User = mongoose.model("User", userSchema);
 
-
+module.exports = User;
 
 
