e1e9ef0d53976ac85ea091ad3979531e28c53e67
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-12-03 08:09:42 -0300

    Agreagndo Routes en el frontend

--- Test1/frontend/src/App.jsx
+++ Test1/frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import CreateUser from "./components/CreateUser";
 import ReadUser from "./components/ReadUser";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <>
       <h2>Frontend</h2>
-      <CreateUser /> 
-      <ReadUser />
+      <Routes>
+        <Route path="/" element={<ReadUser/>}/>
+        <Route path="/createUser" element={<CreateUser />}/>
+      </Routes>
+
+
+
     </>
   )
 }
